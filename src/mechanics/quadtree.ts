00d137bfa9b57316f3871f8f00d1a6f03b6ca23d
grnch <grnch.net@gmail.com>
2021-02-11 18:51:50 +0200

    [src] mechanics: update QuadTree (change axis)

--- src/mechanics/quadtree.ts
+++ src/mechanics/quadtree.ts
@@ -4,11 +4,11 @@ import type {
 
 interface Rect {
   x1: number;
-  y1: number;
+  z1: number;
   x2: number;
-  y2: number;
+  z2: number;
   xMid?: number;
-  yMid?: number;
+  zMid?: number;
   half?: number;
 }
 
@@ -39,7 +39,7 @@ class QuadTree<T extends TreeObject> {
       ++level;
       binary *= 2;
     }
-    const bound = { x1: 0, y1: 0, x2: binary, y2: binary };
+    const bound = { x1: 0, z1: 0, x2: binary, z2: binary };
     this.map = [];
     this._initialize(bound, level, this.map);
     const sector = this.map[0][0].bound;
@@ -60,7 +60,7 @@ class QuadTree<T extends TreeObject> {
     this.parent = parent;
 
     bound.xMid = (bound.x1 + bound.x2) * 0.5;
-    bound.yMid = (bound.y1 + bound.y2) * 0.5;
+    bound.zMid = (bound.z1 + bound.z2) * 0.5;
     const size = bound.x2 - bound.x1;
     bound.half = size * 0.5;
 
@@ -70,7 +70,7 @@ class QuadTree<T extends TreeObject> {
       this._objects = [];
       this.static_objects = [];
       const column = this.bound.x1 / size;
-      const row = this.bound.y1 / size;
+      const row = this.bound.z1 / size;
       map[column] = map[column] || [];
       map[column][row] = this;
     }
@@ -82,13 +82,13 @@ class QuadTree<T extends TreeObject> {
     map: QuadTree<T>[][]
   ) {
     const level = this._level - 1;
-    const { x1, y1, x2, y2, xMid, yMid } = this.bound;
+    const { x1, z1, x2, z2, xMid, zMid } = this.bound;
 
     this._nodes = [
-      new QuadTree<T>()._initialize({ x1: x1, y1: y1, x2: xMid, y2: yMid }, level, map, this),
-      new QuadTree<T>()._initialize({ x1: xMid, y1: y1, x2: x2, y2: yMid }, level, map, this),
-      new QuadTree<T>()._initialize({ x1: x1, y1: yMid, x2: xMid, y2: y2 }, level, map, this),
-      new QuadTree<T>()._initialize({ x1: xMid, y1: yMid, x2: x2, y2: y2 }, level, map, this)
+      new QuadTree<T>()._initialize({ x1: x1, z1: z1, x2: xMid, z2: zMid }, level, map, this),
+      new QuadTree<T>()._initialize({ x1: xMid, z1: z1, x2: x2, z2: zMid }, level, map, this),
+      new QuadTree<T>()._initialize({ x1: x1, z1: zMid, x2: xMid, z2: z2 }, level, map, this),
+      new QuadTree<T>()._initialize({ x1: xMid, z1: zMid, x2: x2, z2: z2 }, level, map, this)
     ];
   }
 
@@ -96,7 +96,7 @@ class QuadTree<T extends TreeObject> {
     object: T
   ) {
     const column = Math.floor(object.position.x / this._sector);
-    const row = Math.floor(object.position.y / this._sector);
+    const row = Math.floor(object.position.z / this._sector);
     this.map[column][row].add(object);
   }
 
@@ -104,7 +104,7 @@ class QuadTree<T extends TreeObject> {
     object: T
   ) {
     const column = Math.floor(object.position.x / this._sector);
-    const row = Math.floor(object.position.y / this._sector);
+    const row = Math.floor(object.position.z / this._sector);
     this.map[column][row].addStatic(object);
   }
 
@@ -148,10 +148,10 @@ class QuadTree<T extends TreeObject> {
       return;
     }
 
-    const { xMid, yMid, half } = this.bound;
+    const { xMid, zMid, half } = this.bound;
     const length = half + radius;
     if (Math.abs(xMid - point.x) > length
-      || Math.abs(yMid - point.y) > length
+      || Math.abs(zMid - point.z) > length
     ) {
       return;
     }
