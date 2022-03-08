22e5a69118a2c764da2bd85e841ef318f0772bc1
Gulevich Eugene <grnch.net@gmail.com>
2022-03-08 22:45:01 +0200

    [src] cluster: update client (refactor, add scene)

--- cluster/client/src/main.js
+++ cluster/client/src/main.js
@@ -24,10 +24,14 @@
   const world_characters_node = document.querySelector('#world-characters');
   const character_leave_node = document.querySelector('#character-leave');
   const character_cancel_leave_node = document.querySelector('#character-cancel-leave');
-  const character_your_node = document.querySelector('#character-your');
+  const user_character_name_node = document.querySelector('#user-character-name');
   const world_chat_node = document.querySelector('#world-chat');
   const character_say_node = document.querySelector('#character-say');
   const character_say_text_node = document.querySelector('#character-say-text');
+  const world_scene_node = document.querySelector('#world-scene');
+
+  const world_width = 180;
+  const world_height = 180;
 
   class Game {
 
@@ -36,6 +40,7 @@
       this.worldData = null;
       this.worldIndex = null;
       this.socket = null;
+      this.sceneCharacters = null;
     }
 
     initialize() {
@@ -103,30 +108,13 @@
       this.character = data.parameters;
     }
 
-    updateWorldCharacters() {
-      const characters_list = [];
-      for (const character of this.worldData.characters) {
-        if (!character) {
-          continue;
-        }
-        characters_list.push(character.name);
-      }
-      world_characters_node.value = characters_list.join(', ');
-    }
-
-    initWorld() {
-      main_screen_node.style.display = 'none';
-      world_screen_node.style.display = '';
-      character_your_node.value = this.character.name;
-      character_leave_node.style.display = '';
-      character_cancel_leave_node.style.display = 'none';
-      this.updateWorldCharacters();
-    }
-
     async enterToWorld(characterName) {
+      character_enter_node.style.display = 'none';
+
       if (!this.character || this.character.name !== characterName) {
         await this.getCharacter(characterName);
         if (!this.character) {
+          this.thenEnterToWorld();
           return;
         }
       }
@@ -134,6 +122,7 @@
       const enter_data = await this.sendRequest(APIEvent.WorldEnter, { name: characterName });
       console.log('enter to world', enter_data);
       if (!enter_data) {
+        this.thenEnterToWorld();
         return;
       }
   
@@ -169,26 +158,95 @@
       this.socket.on(WSEvent.CharEnter, data => {
         console.log('Char enter to world', data);
         this.worldData.characters[data.worldIndex] = data.characterData;
-        this.updateWorldCharacters();
+        this.updateWorldCharactersList();
+        this.addCharacter(data.worldIndex, data.characterData);
       });
 
       this.socket.on(WSEvent.CharLeave, data => {  
         if (this.worldIndex == data.worldIndex) {
           console.warn('Your char leave from world');
           this.socket.disconnect();
-          main_screen_node.style.display = '';
-          world_screen_node.style.display = 'none';
-          character_your_node.value = '';
+          this.destroyWorld();
           return;
         }
   
-        const characterData = this.worldData.characters[data.worldIndex];
-        console.log('Char leave from world', characterData?.name);
-        delete this.worldData.characters[data.worldIndex];
-        this.updateWorldCharacters();
+        this.removeCharacter(data.worldIndex);
       });
       
       this.socket.emit(WSEvent.CharEnter, { secret: enter_data.secret });
+      this.thenEnterToWorld();
+    }
+
+    thenEnterToWorld() {
+      character_enter_node.style.display = '';
+    }
+
+    updateWorldCharactersList() {
+      const characters_list = [];
+      for (const character of this.worldData.characters) {
+        if (!character) {
+          continue;
+        }
+        characters_list.push(character.name);
+      }
+      world_characters_node.value = characters_list.join(', ');
+    }
+
+    initWorld() {
+      main_screen_node.style.display = 'none';
+      world_screen_node.style.display = '';
+      user_character_name_node.value = this.character.name;
+      character_leave_node.style.display = '';
+      character_cancel_leave_node.style.display = 'none';
+      this.updateWorldCharactersList();
+      this.initWorldScene();
+    }
+
+    initWorldScene() {
+      this.sceneCharacters = [];
+
+      this.addUserCharacter();
+
+      for (const worldIndex in this.worldData.characters) {
+        const character = this.worldData.characters[worldIndex];
+        if (!character) {
+          return;
+        }
+        this.addCharacter(worldIndex, character);
+      }
+    }
+
+    addUserCharacter() {
+      const character_node = this.createPlayer(this.character, '#user-character-prefab');
+      this.sceneCharacters[this.worldIndex] = character_node;
+    }
+
+    addCharacter(worldIndex, character) {
+      const character_node = this.createPlayer(character);
+      this.sceneCharacters[worldIndex] = character_node;
+    }
+
+    removeCharacter(worldIndex) {
+      const characterData = this.worldData.characters[worldIndex];
+      console.log('Char leave from world', characterData?.name);
+
+      delete this.worldData.characters[worldIndex];
+      this.updateWorldCharactersList();
+
+      const character_node = this.sceneCharacters[worldIndex];
+      delete this.sceneCharacters[worldIndex];
+      world_scene_node.removeChild(character_node);
+    }
+
+    createPlayer(character, prefab = '#character-prefab') {
+      const player_node = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+      player_node.setAttributeNS('http://www.w3.org/1999/xlink', 'href', prefab);
+      // const { x, y, z } = character.position;
+      const x = Math.random() * world_width;
+      const z = Math.random() * world_height;
+      player_node.setAttribute('transform', `translate(${x}, ${world_height - z})`);
+      world_scene_node.appendChild(player_node);
+      return player_node;
     }
 
     leaveFromWorld() {
@@ -201,6 +259,15 @@
       character_leave_node.style.display = '';
       character_cancel_leave_node.style.display = 'none';
       this.socket.emit(WSEvent.CharCancelLeave);
+    }
+
+    destroyWorld() {
+      main_screen_node.style.display = '';
+      world_screen_node.style.display = 'none';
+      user_character_name_node.value = '';
+      while (world_scene_node.firstChild) {
+        world_scene_node.removeChild(world_scene_node.lastChild);
+      }
     }
 
     characterSay() {
